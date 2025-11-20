d3b2e6ae0d9067e0be34947aff0d0165e68ad2fe
Tadas Šimkus <tadas.simkus97@gmail.com>
2025-11-20 16:40:51 +0000

    refactor: add some window styling

--- lua/taysim/sets.lua
+++ lua/taysim/sets.lua
@@ -61,4 +61,29 @@ vim.opt.splitbelow = true
 vim.opt.inccommand = 'split'
 
 -- Enable errors and warning virtual text
-vim.diagnostic.config { virtual_text = true }
+vim.diagnostic.config {
+  -- virtual_lines = true,
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+    },
+  },
+}
+
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.winborder = 'rounded'
