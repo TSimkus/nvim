b9ff62fdd2460adfadb657cd1adbf245c5d30b26
Tadas Šimkus <tadas.simkus97@gmail.com>
2026-04-18 16:37:53 +0000

    feat: add lazydev.nvim

--- lua/taysim/plugins/lsp.lua
+++ lua/taysim/plugins/lsp.lua
@@ -11,10 +11,6 @@ return {
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
@@ -127,6 +123,9 @@ return {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
+              },
+              diagnostics = {
+                globals = { 'vim' },
               },
             },
           },
